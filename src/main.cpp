bc96ebe6cb140e10559211f14cf220b7e01f2b18
Tahi-knct <ynagiz22@gmail.com>
2021-02-05 18:12:54 +0900

    Set Format

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
+#include <Adafruit_BNO055.h>
+#include <Adafruit_Sensor.h>
 #include <Arduino.h>
+#include <Wire.h>
+#include <utility/imumaths.h>
+
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 100;
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    if (!bno.begin()) {
+        Serial.print("No BNO055 detected");
+        while (1)
+            ;
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() {}
