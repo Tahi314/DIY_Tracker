333f307aa7a383070879abf749a832e3333da1d0
Tahi-knct <ynagiz22@gmail.com>
2021-03-20 19:04:53 +0900

    send serial add

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,10 @@ Adafruit_BNO055 bno[4] = Adafruit_BNO055(55, 0x70);
 
 BluetoothSerial SerialBT;
 
-void TCA9548A(uint8_t bus);
-void read_sensor(uint8_t num);
-void send_serial(float qx, float qy, float qz, float qw);
+void SwitchI2C(uint8_t bus);  //TCA9548A
+void send_serial(float inDataArray[20]);
+
+float QuatDataArray[20];
 
 void setup() {
     Serial.begin(115200);
@@ -19,7 +20,7 @@ void setup() {
 
     for (int i = 0; i < 5; i++) {
         if (!bno[i].begin(bno->OPERATION_MODE_IMUPLUS)) {
-            Serial.print("BNO055_");
+            Serial.print("BNO055_No.");
             Serial.print(i);
             Serial.print(" not detected\n");
             while (1)
@@ -30,55 +31,27 @@ void setup() {
 
 void loop() {
     for (int i = 0; i < 5; i++) {
-        TCA9548A(i);
-        read_sensor(i);
+        SwitchI2C(i);
+        imu::Quaternion quat = bno[i].getQuat();
+        QuatDataArray[i * 4] = quat.x();
+        QuatDataArray[i * 4 + 1] = quat.y();
+        QuatDataArray[i * 4 + 2] = quat.z();
+        QuatDataArray[i * 4 + 3] = quat.w();
     }
+    send_serial(QuatDataArray);
 }
 
-void TCA9548A(uint8_t bus) {
+void SwitchI2C(uint8_t bus) {
     Wire.beginTransmission(0x70);
     Wire.write(1 << bus);
     Wire.endTransmission();
 }
 
-void read_sensor(uint8_t num) {
-    imu::Quaternion quat = bno[num].getQuat();
-    send_serial(quat.x(), quat.y(), quat.z(), quat.w());
-}
-
-void send_serial(float qx, float qy, float qz, float qw) {
-    Serial.print("s\t");
-    Serial.print(qx);
-    Serial.print("\t");
-    Serial.print(qy);
-    Serial.print("\t");
-    Serial.print(qz);
-    Serial.print("\t");
-    Serial.println(qw);
-}
-
-/*void send_serial(float qx, float qy, float qz, float qw) {
-    if (Serial.available() == 1) {
-        byte inBuf[1];
-        Serial.readBytes(inBuf, 1);
-        if (inBuf[0] == 's') {
-            byte outBuf[10];
-            outBuf[0] = 's';
-            outBuf[1] = (int16_t)(qx * 100) >> 8;
-            outBuf[2] = (int16_t)(qx * 100) & 0xFF;
-            outBuf[3] = (int16_t)(qy * 100) >> 8;
-            outBuf[4] = (int16_t)(qy * 100) & 0xFF;
-            outBuf[5] = (int16_t)(qz * 100) >> 8;
-            outBuf[6] = (int16_t)(qz * 100) & 0xFF;
-            outBuf[7] = (int16_t)(qw * 100) >> 8;
-            outBuf[8] = (int16_t)(qw * 100) & 0xFF;
-            outBuf[9] = 'e';
-            Serial.write(outBuf, 10);
-        } else {
-            while (Serial.available() > 0) Serial.read();
-        }
+void send_serial(float inDataArray[20]) {
+    SerialBT.print("s\t");
+    for (int i = 0; i < 20; i++) {
+        SerialBT.print(QuatDataArray[i]);
+        SerialBT.print("\t");
     }
-    if (Serial.available() > 1) {
-        while (Serial.available() > 0) Serial.read();
-    }
-}*/
+    SerialBT.print("\n");
+}
